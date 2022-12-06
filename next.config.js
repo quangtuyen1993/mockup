ae9b5f9427882168d9f6c29e3e3da0e21e89bd63
Bellamy Nguyen <Bellamy.Nguyen@legato.co>
2022-12-07 01:32:59 +0700

    aaa

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const { default: next } = require('next')
+
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+    /* config optimages.unoptimized = trueons here */
+
+  }
+  images.unoptimized = true  
+  module.exports = nextConfig
