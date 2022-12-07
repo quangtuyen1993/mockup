21fdc28aea50519715eb7413d512f0e4c0043102
Bellamy Nguyen <Bellamy.Nguyen@legato.co>
2022-12-07 13:05:43 +0700

    aaa

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
@@ -26,19 +25,12 @@ export default function Home({ allPostsData }) {
       </section>
       <section className={utilStyles.headingMd}>
         <p>
-          https://mockup-git-main-pizza-legato.vercel.app/api endpoint
+          <a href='  https://mockup-git-main-pizza-legato.vercel.app/api'>
+          https://mockup-git-main-pizza-legato.vercel.app/api
+          </a>
         </p>
       </section>
 
     </Layout>
   )
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
 }
