cae2ed46e3b401762a94390300d272225210a415
Bellamy Nguyen <Bellamy.Nguyen@legato.co>
2022-12-07 10:52:05 +0700

    remove middeware

--- middleware.js
+++
@@ -1,7 +0,0 @@
-import  { NextRequest ,NextResponse} from 'next/server'
-
-export function middleware(request) {
-  const response = NextResponse.next()
-  console.log(response)
-  return response;
-}
