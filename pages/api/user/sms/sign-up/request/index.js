f0404cff5868faf56f01307e79fc07f45503e2a2
Bellamy Nguyen <Bellamy.Nguyen@legato.co>
2022-12-07 11:03:57 +0700

    oio

--- pages/api/user/sms/sign-up/request/index.js
+++ pages/api/user/sms/sign-up/request/index.js
@@ -1,7 +1,8 @@
 import { faker } from "@faker-js/faker";
+import okMessage from "../../../../../../lib/okmessage";
 
 export default function handler(req, res) {
-    const otp={
+    const otp = {
     otp:faker.finance.pin(6)
 }
     res.status(200).json(okMessage(otp));
