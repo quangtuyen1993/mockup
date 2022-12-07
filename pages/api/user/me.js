f453d112472b233c3e1a58301b082e13ddeb790b
Bellamy Nguyen <Bellamy.Nguyen@legato.co>
2022-12-07 15:18:16 +0700

    aaa

--- pages/api/user/me.js
+++ pages/api/user/me.js
@@ -1,3 +1,24 @@
+import { faker } from "@faker-js/faker";
+import okMessage from "../../../lib/okmessage";
+
 export default function handler(req, res) {
-    res.status(200).json({ "username":"bellamy","avatar":"dep zai","description":"ngon zai" });
+  const userMe={
+        first_name: faker.name.firstName(),
+        last_name: faker.name.lastName(),
+        email: faker.internet.email(),
+        district_id: faker.datatype.number(10),
+        gender: faker.datatype.number(1),
+        age_group: faker.datatype.number(30),
+        phone_no: faker.phone.phoneNumber(),
+        birthday: faker.date.birthdate(),
+        created_at: faker.date.past(),
+        member_no: faker.datatype.number(),
+        membership_name: faker.finance.accountName(),
+        point: faker.datatype.number(),
+        spending: faker.datatype.number(),
+        expired_at: faker.date.future(),
+        min:faker.datatype.number(10),
+        max:faker.datatype.number(100),
+  }
+    res.status(200).json(okMessage(userMe));
   }
