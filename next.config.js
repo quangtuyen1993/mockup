20176ec1e6c55d75766020d0f729a390c92fde3f
Bellamy Nguyen <Bellamy.Nguyen@legato.co>
2022-12-07 10:51:10 +0700

    aaa

--- next.config.js
+++ next.config.js
@@ -6,12 +6,5 @@ const { default: next } = require('next')
  const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    allowMiddlewareResponseBody: true,
-  },
-  images: {
-    unoptimized: true,
-
-  },
   }
   module.exports = nextConfig
