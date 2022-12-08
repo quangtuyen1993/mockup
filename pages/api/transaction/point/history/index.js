6898c608605ad4247f25c818636baa8f8f3061c3
Bellamy Nguyen <Bellamy.Nguyen@legato.co>
2022-12-08 08:43:04 +0700

    created_at

--- pages/api/transaction/point/history/index.js
+++ pages/api/transaction/point/history/index.js
@@ -3,7 +3,7 @@ import okMessage, { okMessagePaging } from "../../../../../lib/okmessage";
 function spending(){
     return {
         id:faker.datatype.number(),
-        create_at:faker.date.recent(),
+        created_at:faker.date.recent(),
         point :faker.datatype.number(100000),
         coupon_name:faker.random.word()
     }
