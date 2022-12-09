7b29c5a49f8d267b754e6c0b97cfae26aa84f816
Bravo <huynh.tinh@legato.co>
2022-12-09 16:44:17 +0700

    register response

--- pages/api/user/sms/sign-up/index.ts
+++ pages/api/user/sms/sign-up/index.ts
@@ -2,8 +2,8 @@ import {
     NextApiRequest,
     NextApiResponse
 } from './../../../../../node_modules/next/dist/shared/lib/utils.d';
-import { emptySuccess } from '../../../../../lib/DataResult';
+import { emptySuccess, onLoginSuccess } from '../../../../../lib/DataResult';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json(emptySuccess());
+    res.status(200).json(onLoginSuccess());
 }
