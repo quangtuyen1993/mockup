9a53005d1fb17a41c370ada99eba9a23309f4b69
Bravo <huynh.tinh@legato.co>
2022-12-12 17:43:35 +0700

    add delay

--- pages/api/user/sms/sign-up/index.ts
+++ pages/api/user/sms/sign-up/index.ts
@@ -5,9 +5,13 @@ import {
 import { onLoginSuccess } from '../../../../../lib/data_result';
 import { withSafeRequest } from '../../../../../lib/with_safe_request';
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+    await delay(5000);
     res.status(200).json(onLoginSuccess());
 }
 
 export default withSafeRequest(handler)
 
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
