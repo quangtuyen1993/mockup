399b9bc83bf61d0deed4a2a08a3764bc5016cb39
Bellamy Nguyen <Bellamy.Nguyen@legato.co>
2022-12-13 11:08:04 +0700

    change lang

--- lib/with_safe_request.ts
+++ lib/with_safe_request.ts
@@ -2,9 +2,21 @@ import { faker } from '@faker-js/faker';
 import { NextApiHandler } from 'next';
 import { error } from './data_result';
 
-const withSafeRequest =(handler: NextApiHandler): NextApiHandler =>
+const withSafeRequest =
+    (handler: NextApiHandler): NextApiHandler =>
     async (req, res) => {
         try {
+            var { langParams } = req.query;
+
+            var lang;
+            if (langParams == 'zh_cn') {
+                lang = 'zh_CN';
+            } else if ((langParams = 'zh_hk')) {
+                lang = 'zh_TW';
+            } else {
+                lang = 'en';
+            }
+            faker.setLocale(lang)
             await handler(req, res);
         } catch (e) {
             res.status(500).json(error(`${e}`));
