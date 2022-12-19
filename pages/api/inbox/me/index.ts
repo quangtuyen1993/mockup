59878aad8d047dd77926b385fec2a5a68b332196
Bravo <huynh.tinh@legato.co>
2022-12-19 17:54:54 +0700

    inbox api

---
+++ pages/api/inbox/me/index.ts
@@ -0,0 +1,37 @@
+import { DataResult, EmptySuccess, emptySuccess } from '../../../../lib/data_result';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { withSafeRequest } from '../../../../lib/with_safe_request';
+import { StringColorFormat } from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
+
+type Inbox = {
+    id: string;
+    type: number;
+    title: string;
+    body: string;
+    read_at: string | null;
+    created_at: String;
+};
+
+function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<DataResult<EmptySuccess>>
+) {
+    
+    res.status(200).json(emptySuccess());
+}
+
+
+function _item() {
+    return {
+        id: faker.datatype.number().toString(),
+        type: 1,
+        title: faker.lorem.word(),
+        body: faker.lorem.sentence(),
+        read_at: faker.date.past,
+        created_at : faker.date.past
+    };
+}
+
+
+export default withSafeRequest(handler)
