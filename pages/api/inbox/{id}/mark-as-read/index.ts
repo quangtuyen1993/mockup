ffb652dce3644b34eaa9876c1e00f9cc6a8c7d64
Bravo <huynh.tinh@legato.co>
2022-12-21 14:11:59 +0700

    inbox read api

---
+++ pages/api/inbox/{id}/mark-as-read/index.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next/dist/shared/lib/utils";
+import { emptySuccess } from "../../../../../lib/data_result";
+import { withSafeRequest } from "../../../../../lib/with_safe_request";
+
+
+function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.status(200).json(emptySuccess());
+}
+
+export default withSafeRequest(handler)
